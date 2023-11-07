48bbeceade72203b44feb373585252c0ae40d542
Tadashi Nemoto <tadashi.nemoto0713@gmail.com>
2023-11-07 09:54:58 +0900

    node/test

--- jest.config.js
+++ jest.config.js
@@ -8,6 +8,7 @@ const createJestConfig = nextJest({
 const customJestConfig = {
   moduleDirectories: ['node_modules', '<rootDir>/'],
   testEnvironment: 'jest-environment-jsdom',
+  reporters: [ 'default', 'jest-junit' ],
 }
 
 module.exports = createJestConfig(customJestConfig)
