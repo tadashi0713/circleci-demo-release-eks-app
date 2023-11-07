e10e1a5bd9899b21d0c6c7f9f5dfce4d60fa91c0
Tadashi Nemoto <tadashi.nemoto0713@gmail.com>
2023-11-07 10:35:31 +0900

    toHaveTextContent

--- jest.config.js
+++ jest.config.js
@@ -9,6 +9,7 @@ const customJestConfig = {
   moduleDirectories: ['node_modules', '<rootDir>/'],
   testEnvironment: 'jest-environment-jsdom',
   reporters: [ 'default', 'jest-junit' ],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js']
 }
 
 module.exports = createJestConfig(customJestConfig)
