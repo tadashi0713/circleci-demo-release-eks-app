fbb9e8074c8046db2f362db597924142e3eb6fa8
Tadashi Nemoto <tadashi.nemoto0713@gmail.com>
2023-11-07 09:32:18 +0900

    Run juest

---
+++ src/app/page.test.tsx
@@ -0,0 +1,11 @@
+import { render, screen, fireEvent} from '@testing-library/react';
+import Home from './page';
+
+describe('Click button', () => {
+  render(<Home />);
+  fireEvent.click(screen.getByTestId("Button"));
+
+  test('Text should be shown', () => {
+    expect(screen.getByTestId('Text').textContent).toEqual('Hello');
+  });
+});
